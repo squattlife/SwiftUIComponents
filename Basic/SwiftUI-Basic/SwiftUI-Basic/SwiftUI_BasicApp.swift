e0ec1db1721abef3f4123e63ffc82c33028e59fb
redwine12 <migonyoung01@gmail.com>
2023-02-06 14:07:45 +0900

    Initial commit

---
+++ Basic/SwiftUI-Basic/SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by 이재영 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
